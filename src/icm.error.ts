b535f39f101fd50ac522d4f60b47560bff8a7662
Luis Bastidas <luis@kabany.io>
2025-08-14 21:09:59 -0600

    feat: base library

---
+++ src/icm.error.ts
@@ -0,0 +1,12 @@
+import { CoreError } from 'tydet-core'
+
+export class IcmError extends CoreError {
+
+  constructor(message?: string, sql?: string) {
+    super();
+    Object.setPrototypeOf(this, IcmError.prototype);
+    this.name = this.constructor.name
+    this.message = message
+    if (Error.captureStackTrace) Error.captureStackTrace(this, IcmError);
+  }
+}
---
+++ src/icm.service.ts
@@ -0,0 +1,10 @@
+export interface KabanyIcmFile {
+  type: string
+  private_key_id: number
+  private_key: string
+  access_domain: string
+  auth_url: string
+}
+
+
+
